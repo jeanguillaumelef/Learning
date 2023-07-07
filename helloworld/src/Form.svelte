a50a2807774c364056ef326dec1bd722025a7bb0
jeanguillaumelef <jeanguillaumelefebvre@gmail.com>
2023-07-07 15:18:40 +0100

    added svelte stuff and modified conf

---
+++ helloworld/src/Form.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    let textboxValue = '';
+  
+    async function saveText() {
+      // Send the textbox value to the API defined in worker.ts
+      try {
+        const response = await fetch('https://helloworld.jeanguillaumelefebvre.workers.dev/api/worker', {
+          method: 'POST',
+          body: JSON.stringify({ text: textboxValue }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        const data = await response.json();
+  
+        // Handle the API response here
+        console.log(data);
+      } catch (error) {
+        // Handle any errors that occurred during the API call
+        console.error(error);
+      }
+    }
+  </script>
+  
+  <main>
+    <h1>Form Page</h1>
+    <input type="text" bind:value={textboxValue} placeholder="Enter text" />
+    <button on:click={saveText}>Save</button>
+  </main>
+  
+  <style>
+    main {
+      padding: 2rem;
+    }
+  </style>
+  
--- helloworld/src/worker.ts
+++ helloworld/src/worker.ts
@@ -1,32 +1,28 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// worker.ts
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
-
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World2!');
-	},
-};
+type RequestEvent = {
+	request: Request;
+  };
+  
+  addEventListener('fetch', (event: FetchEvent) => {
+	event.respondWith(handleRequest(event));
+  });
+  
+  async function handleRequest(event: FetchEvent) {
+	const { request } = event;
+  
+	if (request.method !== 'POST') {
+	  return new Response(`Invalid method: ${request.method}`, { status: 405 });
+	}
+  
+	const data = await request.json();
+  
+	// Process the data or perform any desired operations
+  
+	// Send a response back to the client
+	return new Response(JSON.stringify({ message: 'Text successfully saved!' }), {
+	  headers: { 'Content-Type': 'application/json' },
+	  status: 200,
+	});
+  }
+  
